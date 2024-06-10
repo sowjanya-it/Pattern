b9cc2299d40b18aef70a8ee7a22a306350e807dc
sowjanya-it <159770816+sowjanya-it@users.noreply.github.com>
2024-06-10 13:30:32 +0530

    pattern file added

---
+++ PatternDemo1.java
@@ -0,0 +1,16 @@
+class PatternDemo1
+{
+public static void main(String args[])
+{
+int n=Integer.parseInt(args[0]);
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(i+"\t");
+}
+System.out.println();
+}
+}
+}
+
